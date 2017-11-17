99cbe35778154c5c155942cb85481b6b77193cca
tatsu <tatsu@hotmail.fr>
2017-11-17 17:18:02 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const uuidV4 = require('uuid.v4')
+
+const app = express()
+let todoList = [];
+
+app.use(bodyParser.json());
+app.get('/todos', (req, res) => res.send(todoList))
+
+app.get('/todos/:id', (req, res) => {
+  let result = false;
+  todoList.forEach((todo) => {
+      console.log(todo);
+      if(todo.id === req.params.id){
+        result = todo;
+      }
+  });
+  return res.send(result);
+})
+
+app.post('/todos', (req, res) => {
+  const todo = {
+    id: uuidV4(),
+    name: req.body.name,
+    description: req.body.description,
+  }
+  todoList.push(todo);
+  return res.send(todo);
+});
+
+app.put('/todos/:id', function (req, res) {
+  const id = req.params.id;
+  const newtodo = {
+    id,
+    name: req.body.name,
+    description: req.body.description,
+  };
+
+  todoList = todoList.map((todo) => {
+      if (todo.id === id) {
+        return newtodo;
+      }
+      return todo;
+    });
+  return res.send(true);
+})
+
+app.delete('/todos/:id', function (req, res) {
+  todoList = todoList.filter((todo) => todo.id === req.params.id);
+  return res.send(true);
+})
+
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
